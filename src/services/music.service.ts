eba613ff73d6c98dd3fcdc6cc0af8b0ab17c784a
kiss8981 <kdh89811@gmail.com>
2022-09-01 10:18:56 +0900

    fix: import data formet

--- src/services/music.service.ts
+++ src/services/music.service.ts
@@ -1,14 +1,10 @@
-import { hash, compare } from "bcrypt";
-import { sign } from "jsonwebtoken";
-import { SECRET_KEY } from "@config";
-import { CreateUserDto } from "@dtos/users.dto";
-import { HttpException } from "@exceptions/HttpException";
 import userModel from "@models/users.model";
 import { Request } from "express";
 import { importData, getData } from "@utils/googleSheetAPI";
 import { Music } from "@/dtos/music.dto";
 import musicModel from "@/models/music.model";
 import { getDate } from "@/utils/util";
+import dayjs from "dayjs";
 
 class MusicService {
   public users = userModel;
@@ -16,27 +12,14 @@ class MusicService {
 
   public async addMusic(req: Request): Promise<any> {
     const { deviceId, song } = req.body as Music;
-    const music = await this.musics
-      .find({
-        deviceId,
-      })
-      .sort({ published_date: -1 })
-      .limit(1);
-    if (
-      music.length === 0 ||
-      getDate(new Date(music[0].published_date)) !== getDate(new Date())
-    ) {
-      const musicDB = new this.musics({
-        deviceId,
-        song,
-      });
-      await importData([[deviceId, song]]);
-      await musicDB.save();
-      const musicList = await getData();
-      return musicList.data.values.length;
-    } else {
-      throw new HttpException(400, "오늘은 이미 노래를 신청하셨습니다");
-    }
+    const musicDB = new this.musics({
+      deviceId,
+      song,
+    });
+    await importData([[dayjs(new Date()).locale('ko').format("YYYY. MM. DD A HH:mm:ss"), song, "위 내용을 숙지 하였으며 준수하도록 하겠습니다", deviceId]]);
+    await musicDB.save();
+    const musicList = await getData();
+    return musicList.data.values.length;
   }
 }
 
